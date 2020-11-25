b8cf194ae49bd100232194b72241a01460091b01
Whitney Bluzma <whitney@accommodationforstudents.com>
2020-11-25 13:22:48 +0000

    feat: set up storybook

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,9 @@ import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
+import Avatar from '@material-ui/core/Avatar'
 
 import styles from './header.module.scss'
-import Avatar from '@material-ui/core/Avatar'
 
 const useStyles = makeStyles((theme) => ({
   root: {
