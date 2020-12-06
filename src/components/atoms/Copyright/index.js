d3d33b9160124030b35c007d4a9ac229c7518449
Whitney Bluzma <whitney@accommodationforstudents.com>
2020-12-06 19:06:43 +0000

    feat: add shameless self promotion in the footer

--- src/components/atoms/Copyright/index.js
+++ src/components/atoms/Copyright/index.js
@@ -1,17 +1,28 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import MuiLink from '@material-ui/core/Link'
+import { FavoriteRounded } from '@material-ui/icons'
 
 import styles from './copyright.module.scss'
 
 export default function Copyright() {
   return (
-    <Typography variant="body2" align="center" className={styles.copyright}>
-      {'Copyright © '}
-      <MuiLink color="inherit" href="/">
-        Harri Shanahan
-      </MuiLink>{' '}
-      {new Date().getFullYear()}
-    </Typography>
+    <>
+      <Typography variant="body2" align="center" className={styles.copyright}>
+        {'Copyright © '}
+        <MuiLink color="inherit" href="/">
+          Harri Shanahan
+        </MuiLink>{' '}
+        {new Date().getFullYear()}
+      </Typography>
+      <Typography variant="body2" align="center" className={styles.madeBy}>
+        {'Made with '}
+        <FavoriteRounded className={styles.icon} />
+        {' by '}
+        <MuiLink color="inherit" href="https://github.com/thepunkyone">
+          thepunkyone
+        </MuiLink>
+      </Typography>
+    </>
   )
 }
