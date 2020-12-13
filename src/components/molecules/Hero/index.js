11d48b25bc4f443eadfd4871c82a83f8d5529c5b
Whitney Bluzma <whitney@accommodationforstudents.com>
2020-12-13 13:13:22 +0000

    feat: lazy loading and image size transform for hero image

--- src/components/molecules/Hero/index.js
+++ src/components/molecules/Hero/index.js
@@ -2,15 +2,46 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import Container from '@material-ui/core/Container'
 
+import sourceImageType from '../../../utils/sourceImageType'
+
+import Picture from '../../atoms/Picture'
+
 import styles from './hero.module.scss'
 
 const Hero = () => {
+  const image = './images/hero/my-hero.jpg'
+
+  const fallbackImageSource = `${image}?nf_resize=fit&h=700`
+
+  const sources = [
+    {
+      id: 'desktop',
+      srcSet: image,
+      media: '(min-width: 1301px)',
+      type: sourceImageType(image),
+    },
+    {
+      id: 'tablet',
+      srcSet: `${image}?nf_resize=fit&h=842`,
+      media: '(min-width: 661px) and (max-width: 1300px)',
+      type: sourceImageType(image),
+    },
+    {
+      id: 'mobile',
+      srcSet: `${image}?nf_resize=fit&h=700`,
+      media: '(max-width: 660px)',
+      type: sourceImageType(image),
+    },
+  ]
+
   return (
     <Container maxWidth={false} disableGutters className={styles.hero}>
-      <img
+      <Picture
         className={styles.image}
         alt="Girl riding a bike down a country road while dressed as a fairy tale knight"
-        src="./images/hero/my-hero.jpg"
+        src={fallbackImageSource}
+        sources={sources}
+        lazyload
       />
       <Container maxWidth="lg" className={styles.headingWrapper}>
         <Typography variant="h1" component="h1" className={styles.heading}>
