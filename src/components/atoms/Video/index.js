73d5f6424b800cdb41c4648a0777e024652b2f0e
Whitney Bluzma <whitney@accommodationforstudents.com>
2020-12-16 18:40:20 +0000

    chore: trial gif fallback for safari

--- src/components/atoms/Video/index.js
+++ src/components/atoms/Video/index.js
@@ -13,40 +13,41 @@ export default function Video({ url, alt }) {
   useEffect(() => {
     // check if user agent is safari and we have the ref to the container <div />
     if (isSafari() && videoParentRef.current) {
-      // obtain reference to the video element
-      const player = videoParentRef.current.children[0]
-
-      // if the reference to video player has been obtained
-      if (player) {
-        // set the video attributes using javascript as per the
-        // webkit Policy
-        // player.controls = false
-        // player.playsinline = true
-        // player.muted = true
-        // player.setAttribute('muted', '') // leave no stones unturned :)
-        // player.autoplay = true
-
-        // Let's wait for an event loop tick and be async.
-        setTimeout(() => {
-          // player.play() might return a promise but it's not guaranteed crossbrowser.
-          const promise = player.play()
-          // let's play safe to ensure that if we do have a promise
-          if (promise.then) {
-            promise
-              .then(() => {})
-              .catch(() => {
-                // if promise fails, hide the video and fallback to <img> tag
-                videoParentRef.current.style.display = 'none'
-                setShouldUseImage(true)
-              })
-          }
-        }, 0)
-      }
+      setShouldUseImage(true)
+      // // obtain reference to the video element
+      // const player = videoParentRef.current.children[0]
+      //
+      // // if the reference to video player has been obtained
+      // if (player) {
+      //   // set the video attributes using javascript as per the
+      //   // webkit Policy
+      //   // player.controls = false
+      //   // player.playsinline = true
+      //   // player.muted = true
+      //   // player.setAttribute('muted', '') // leave no stones unturned :)
+      //   // player.autoplay = true
+      //
+      //   // Let's wait for an event loop tick and be async.
+      //   setTimeout(() => {
+      //     // player.play() might return a promise but it's not guaranteed crossbrowser.
+      //     const promise = player.play()
+      //     // let's play safe to ensure that if we do have a promise
+      //     if (promise.then) {
+      //       promise
+      //         .then(() => {})
+      //         .catch(() => {
+      //           // if promise fails, hide the video and fallback to <img> tag
+      //           videoParentRef.current.style.display = 'none'
+      //           setShouldUseImage(true)
+      //         })
+      //     }
+      //   }, 0)
+      // }
     }
   })
 
   return shouldUseImage ? (
-    <img src={url} alt={alt} />
+    <img src="./images/home-gallery/my-hero-2-fallback.gif" alt={alt} />
   ) : (
     <div
       ref={videoParentRef}
@@ -57,6 +58,7 @@ export default function Video({ url, alt }) {
           muted
           autoplay
           playsinline
+          preload="metadata"
         >
         <source src="${url}" type="video/mp4" />
         </video>`,
