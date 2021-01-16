c9b21c65b5404ffc3785cf7d71e68fc8f9f532b6
Whitney Bluzma <whitney@accommodationforstudents.com>
2021-01-16 20:28:51 +0000

    style: styled carousel component

--- src/components/organisms/Carousel/index.js
+++ src/components/organisms/Carousel/index.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
 import {
   CarouselProvider,
   Slider,
@@ -8,40 +10,90 @@ import {
   DotGroup,
   Dot,
 } from 'pure-react-carousel'
+import {
+  ArrowBackIosRounded as ArrowBack,
+  ArrowForwardIosRounded as ArrowForward,
+} from '@material-ui/icons'
+
+import LazyImage from '../../atoms/LazyImage'
 
 import 'pure-react-carousel/dist/react-carousel.es.css'
 import styles from './carousel.module.scss'
 
-const Carousel = () => {
+const Carousel = ({
+  className,
+  slides,
+  naturalSlideWidth,
+  naturalSlideHeight,
+}) => {
+  const [selectedSlide, setSelectedSlide] = useState(0)
   return (
     <CarouselProvider
-      naturalSlideWidth={100}
-      naturalSlideHeight={50}
-      totalSlides={3}
+      className={className}
+      naturalSlideWidth={naturalSlideWidth}
+      naturalSlideHeight={naturalSlideHeight}
+      totalSlides={slides.length}
+      infinite
     >
-      <Slider classNameAnimation={styles.sliderAnimation}>
-        <Slide index={0}>I am the first Slide.</Slide>
-        <Slide index={1}>I am the second Slide.</Slide>
-        <Slide index={2}>I am the third Slide.</Slide>
-      </Slider>
-      <ButtonBack>Back</ButtonBack>
-      <ButtonNext>Next</ButtonNext>
-      <DotGroup
-        renderDots={({ totalSlides }) => {
-          const dots = []
-          for (let i = 0; i < totalSlides; i++) {
-            dots.push(
-              <Dot key={i} slide={i}>
-                haha great {i}
-              </Dot>
+      <div className={styles.sliderWrapper}>
+        <Slider
+          className={styles.slider}
+          classNameAnimation={styles.sliderAnimation}
+        >
+          {slides.map((slide, i) => {
+            return (
+              <Slide index={i} key={slide.thumbnailImage}>
+                {slide.component}
+              </Slide>
             )
-          }
+          })}
+        </Slider>
+        <ButtonBack className={styles.buttonBack}>
+          <ArrowBack className={styles.arrow} />
+        </ButtonBack>
+        <ButtonNext className={styles.buttonNext}>
+          <ArrowForward className={styles.arrow} />
+        </ButtonNext>
+      </div>
+      <div className={styles.thumbnailWrapper}>
+        <DotGroup
+          className={styles.thumbnails}
+          style={{ transform: `translateX(-${selectedSlide * 56}px)` }}
+          renderDots={({ currentSlide }) => {
+            setSelectedSlide(currentSlide)
+            const dots = slides.map((slide, i) => {
+              return (
+                <Dot
+                  className={classNames(styles.thumbnail, {
+                    [styles.thumbnailSelected]: currentSlide === i,
+                  })}
+                  key={slide.thumbnailImage}
+                  slide={i}
+                >
+                  <LazyImage
+                    className={styles.thumbnailImage}
+                    dataSrc={slide.thumbnailImage}
+                    src={slide.thumbnailImage}
+                    alt={`Slide ${i}`}
+                  />
+                </Dot>
+              )
+            })
 
-          return dots
-        }}
-      />
+            return dots
+          }}
+        />
+        <span className={styles.gradient} />
+      </div>
     </CarouselProvider>
   )
+}
+
+Carousel.propTypes = {
+  className: PropTypes.string,
+  slides: PropTypes.array.isRequired,
+  naturalSlideWidth: PropTypes.number.isRequired,
+  naturalSlideHeight: PropTypes.number.isRequired,
 }
 
 export default Carousel
--- src/components/organisms/Carousel/index.stories.js
+++ src/components/organisms/Carousel/index.stories.js
@@ -1,9 +1,57 @@
 import React from 'react'
 
 import Carousel from './index'
+import LazyImage from '../../atoms/LazyImage'
+
+const componentStyles = {
+  width: '100%',
+  height: '100%',
+  objectFit: 'cover',
+}
+
+const slides = [
+  {
+    component: (
+      <LazyImage
+        style={componentStyles}
+        dataSrc="https://images.unsplash.com/photo-1516655855035-d5215bcb5604?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80"
+        src="https://images.unsplash.com/photo-1516655855035-d5215bcb5604?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80"
+        alt="Slide 1"
+      />
+    ),
+    thumbnailImage:
+      'https://images.unsplash.com/photo-1516655855035-d5215bcb5604?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+  },
+  {
+    component: (
+      <LazyImage
+        style={componentStyles}
+        dataSrc="https://images.unsplash.com/photo-1477346611705-65d1883cee1e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80"
+        src="https://images.unsplash.com/photo-1477346611705-65d1883cee1e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80"
+        alt="Slide 2"
+      />
+    ),
+    thumbnailImage:
+      'https://images.unsplash.com/photo-1477346611705-65d1883cee1e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+  },
+  {
+    component: (
+      <LazyImage
+        style={componentStyles}
+        dataSrc="https://images.unsplash.com/photo-1517021897933-0e0319cfbc28?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=719&q=80"
+        src="https://images.unsplash.com/photo-1517021897933-0e0319cfbc28?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=719&q=80"
+        alt="Slide 3"
+      />
+    ),
+    thumbnailImage:
+      'https://images.unsplash.com/photo-1517021897933-0e0319cfbc28?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=719&q=80',
+  },
+]
 
 export default {
   title: 'Organisms/Carousel',
 }
 
-export const defaultCarousel = () => <Carousel />
+export const defaultCarousel = () => (
+  <Carousel slides={slides} naturalSlideWidth={100} naturalSlideHeight={56} />
+)
